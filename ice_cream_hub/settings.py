0212d45c6da7100fb1689e796ec7299e53fc88ee
Himanshu Yadav <39944180+himanshu263@users.noreply.github.com>
2025-07-22 19:05:09 +0530

    fix: static and media setup

--- ice_cream_hub/settings.py
+++ ice_cream_hub/settings.py
@@ -97,5 +97,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
