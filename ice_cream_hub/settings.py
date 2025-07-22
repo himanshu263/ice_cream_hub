c8d5f4c5839619a59984ddd0b4c39d086e859684
Himanshu Yadav <39944180+himanshu263@users.noreply.github.com>
2025-07-22 17:40:21 +0530

    Update settings.py

--- ice_cream_hub/settings.py
+++ ice_cream_hub/settings.py
@@ -12,7 +12,13 @@ SECRET_KEY = 'django-insecure-your-key'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+DEBUG = False
+
+ALLOWED_HOSTS = ['ice-cream-hub.onrender.com', '127.0.0.1', 'localhost']
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://ice-cream-hub.onrender.com"
+]
 
 
 INSTALLED_APPS = [
